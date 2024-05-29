40528dd4f13be608ae473e2fb9dcc4278e3d69af
wikto <wiktor.labno@edu.uekat.pl>
2024-05-29 13:52:26 +0200

    invalid email or login exception

--- authenticate.php
+++ authenticate.php
@@ -15,7 +15,9 @@ if ( mysqli_connect_errno() ) {
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
 if ( !isset($_POST['username'], $_POST['password']) ) {
 	// Could not get the data that should have been sent.
-	exit('Please fill both the username and password fields!');
+	$_SESSION['error'] = 'Incorrect username and/or password!';
+    header('Location: login.php');
+    exit;
 }
 
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
